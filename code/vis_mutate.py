7b03ddcc64b35f4e20e22d17426978f60cb0e485
zeb-smells <bondzeb@gmail.com>
2021-09-06 22:19:06 +0100

    Submission

--- code/vis_mutate.py
+++ code/vis_mutate.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import sys
-
+import numpy as np
 
 
 
@@ -8,11 +8,17 @@ def main(diag = "../data/diagnoses.csv", even = "../data/events.csv"):
     diags = pd.read_csv(diag)
     events = pd.read_csv(even)
 
-    recent_diags = []
+    recent_diags = pd.DataFrame(columns=list(diags.columns))
+
     for ids in diags["EventID"].unique():
         most_recent = max(diags[diags["EventID"] == ids]["DiagnosisDateTime"])
-        most_recent = diags[diags["DiagnosisDateTime"] == most_recent]["Diagnosis"]
-        recent_diags.append(most_recent)
+        most_recent = diags[diags["DiagnosisDateTime"] == most_recent]
+        recent_diags = recent_diags.append(most_recent, ignore_index=False)
 
     print(recent_diags)
-main()
+
+    return None
+
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
